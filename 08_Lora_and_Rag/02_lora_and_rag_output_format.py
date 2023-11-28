7a2fd4547a5e72824602937cf4da4e00f8cfc24d
samswain2 <samswain2256@gmail.com>
2023-11-28 12:46:04 -0600

    Finalized llm+rag testing set

--- 08_Lora_and_Rag/02_lora_and_rag_output_format.py
+++ 08_Lora_and_Rag/02_lora_and_rag_output_format.py
@@ -1,14 +1,16 @@
 """
-This script is designed to transform a CSV file by performing specific data manipulations.
-It changes the names of 'Question' and 'Answer' columns to lowercase, renames the 'llm_answer' column to 'llm_rag_answer',
-and removes the 'URL' column. The script is modular, with each transformation step encapsulated in a separate function 
-for easy maintenance and adaptability.
+This script transforms a CSV file by performing specific data manipulations.
+It changes the names of 'Question' and 'Answer' columns to lowercase, renames 'llm_answer' to 'llm_rag_answer',
+removes the 'URL' column, extracts the relevant part of each answer following "ANSWER:", and cleans up the 'answer' 
+column to form a single paragraph.
 
 Functions:
 - load_csv(file_path): Load a CSV file into a DataFrame.
 - lowercase_column_names(df, columns): Change specified column names in a DataFrame to lowercase.
 - rename_column(df, old_name, new_name): Rename a column in a DataFrame.
 - remove_column(df, column): Remove a column from a DataFrame.
+- extract_answer_part(df, column): Extract the part of each answer following "ANSWER:".
+- clean_answer_column(df, column): Clean up the answer column to form a single paragraph.
 - save_csv(df, file_path): Save a DataFrame to a CSV file.
 - transform_csv(input_path, output_path): Perform the entire transformation process on the CSV file.
 """
@@ -33,6 +35,20 @@ def remove_column(df, column):
     """Remove a column from a DataFrame."""
     return df.drop(column, axis=1)
 
+def split_answer(answer):
+    extracted_ans = answer.split("ANSWER:\n        \n        ")[-1]
+    return extracted_ans
+
+def extract_answer_part(df, column):
+    """Extract the part of each answer following 'ANSWER:'."""
+    df[column] = df[column].apply(lambda x: split_answer(x))
+    return df
+
+def clean_answer_column(df, column):
+    """Clean up the answer column to form a single paragraph."""
+    df[column] = df[column].apply(lambda x: ' '.join(x.split()))
+    return df
+
 def save_csv(df, file_path):
     """Save a DataFrame to a CSV file."""
     df.to_csv(file_path, index=False)
@@ -43,6 +59,8 @@ def transform_csv(input_path, output_path):
     df = lowercase_column_names(df, ['Question', 'Answer'])
     df = rename_column(df, 'llm_answer', 'llm_rag_answer')
     df = remove_column(df, 'URL')
+    df = extract_answer_part(df, 'llm_rag_answer')
+    df = clean_answer_column(df, 'llm_rag_answer')
     save_csv(df, output_path)
 
 # Define file paths
